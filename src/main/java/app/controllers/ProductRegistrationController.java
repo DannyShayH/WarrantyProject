a32f8f1d221ffa9f9b50b43bc5a6788a7809c9b9
DannyShayH <HalawiDD@gmail.com>
2026-03-07 17:52:35 +0100

    refactored ProductRegistrationService/controller

---
+++ src/main/java/app/controllers/ProductRegistrationController.java
@@ -0,0 +1,42 @@
+package app.controllers;
+
+
+import app.dto.ProductRegistrationDTO;
+import app.services.entityServices.ProductRegistrationService;
+import io.javalin.http.Context;
+import jakarta.persistence.EntityManagerFactory;
+
+public class ProductRegistrationController {
+    private final ProductRegistrationService productRegistrationService;
+
+    public ProductRegistrationController(EntityManagerFactory emf){
+        this.productRegistrationService = new ProductRegistrationService(emf);
+    }
+
+    public void create(Context ctx){
+        ProductRegistrationDTO productRegistrationDTO = ctx.bodyAsClass(ProductRegistrationDTO.class);
+        ctx.json(productRegistrationService.create(productRegistrationDTO));
+        ctx.status(201);
+    }
+
+    public void getById(Context ctx){
+        long id = Long.parseLong(ctx.pathParam("id"));
+        ProductRegistrationDTO productRegistrationDTO = productRegistrationService.getByID(id);
+        ctx.json(productRegistrationDTO);
+    }
+
+    public void update(Context ctx){
+        ProductRegistrationDTO productRegistrationDTO = ctx.bodyAsClass(ProductRegistrationDTO.class);
+        ctx.json(productRegistrationService.update(productRegistrationDTO));
+    }
+
+    public void delete(Context ctx){
+        long id = Long.parseLong(ctx.pathParam("id"));
+        productRegistrationService.delete(id);
+        ctx.status(204);
+    }
+
+    public void getAll(Context ctx){
+        ctx.json(productRegistrationService.get());
+    }
+}
--- src/main/java/app/services/entityServices/ProductRegistrationService.java
+++ src/main/java/app/services/entityServices/ProductRegistrationService.java
@@ -1,33 +1,53 @@
 package app.services.entityServices;
 
 import app.daos.ProductRegistrationDAO;
+import app.dto.ProductRegistrationDTO;
 import app.entity.ProductRegistration;
+import app.services.dtoConverter.ProductRegistrationDTOConverter;
 import jakarta.persistence.EntityManagerFactory;
 
 import java.util.Set;
+import java.util.stream.Collectors;
 
-public class ProductRegistrationService implements CrudService<ProductRegistration> {
+public class ProductRegistrationService {
 
     private final ProductRegistrationDAO productRegistrationDAO;
+    private final ProductRegistrationDTOConverter converter;
     private final EntityManagerFactory emf;
 
-    public ProductRegistrationService(EntityManagerFactory emf, ProductRegistrationDAO productRegistrationDAO){
-        this.productRegistrationDAO = productRegistrationDAO;
+    public ProductRegistrationService(EntityManagerFactory emf){
+        this.productRegistrationDAO = new ProductRegistrationDAO(emf);
+        this.converter = new ProductRegistrationDTOConverter(emf);
         this.emf = emf;
     }
 
-    @Override
-    public ProductRegistration create(ProductRegistration productRegistration) {return productRegistrationDAO.create(productRegistration);}
+    public ProductRegistrationDTO create(ProductRegistrationDTO productRegistrationDTO) {
+        ProductRegistration productRegistration = converter.fromDTO(productRegistrationDTO);
+        ProductRegistration created = productRegistrationDAO.create(productRegistration);
+        return converter.toDTO(created);
+    }
 
-    @Override
-    public Set<ProductRegistration> get() {return productRegistrationDAO.get();}
+    public Set<ProductRegistrationDTO> get() {
+        return productRegistrationDAO.get()
+                .stream()
+                .map(converter::toDTO)
+                .collect(Collectors.toSet());
+    }
 
-    @Override
-    public ProductRegistration getByID(Long id) {return productRegistrationDAO.getByID(id);}
+    public ProductRegistrationDTO getByID(Long id) {
+       ProductRegistration productRegistration = productRegistrationDAO.getByID(id);
+       return converter.toDTO(productRegistration);
+    }
 
-    @Override
-    public ProductRegistration update(ProductRegistration productRegistration) {return productRegistrationDAO.update(productRegistration);}
+    public ProductRegistrationDTO update(ProductRegistrationDTO productRegistrationDTO) {
+    ProductRegistration productRegistration = converter.fromDTO(productRegistrationDTO);
+    ProductRegistration updated = productRegistrationDAO.update(productRegistration);
+    return converter.toDTO(updated);
+    }
 
-    @Override
-    public Long delete(ProductRegistration productRegistration) {return productRegistrationDAO.delete(productRegistration);}
+    public Long delete(long id) {
+        ProductRegistration productRegistration = productRegistrationDAO.getByID(id);
+        return productRegistrationDAO.delete(productRegistration);
+
+    }
 }
