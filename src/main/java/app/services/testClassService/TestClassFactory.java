b58ed1f97a5787cb8a071e793c8a7c53007890b6
DannyShayH <HalawiDD@gmail.com>
2026-03-07 20:44:42 +0100

    Testclass sets bidirectional link for product and warranty

--- src/main/java/app/services/testClassService/TestClassFactory.java
+++ src/main/java/app/services/testClassService/TestClassFactory.java
@@ -25,22 +25,25 @@ public class TestClassFactory {
 
     public static void testClassWarranty() {
         User testUser = new User();
+
         testUser.setEmail("danielhalawi22@gmail.com"); // your email
         testUser.setCreatedAt(LocalDateTime.now());
         testUser.setPassword("12345678");
         userDAO.create(testUser);
 
-        Product product = new Product();
-        product.setOwner(testUser);
-        product.setProductName("Test Product");
-        productDAO.create(product);
-
         Warranty testWarranty = new Warranty();
-        testWarranty.setProduct(product);
         testWarranty.setStartDate(LocalDate.now().minusMonths(3));
         testWarranty.setWarrantyMonths(3);
         testWarranty.setEndDate(testWarranty.getStartDate().plusMonths(testWarranty.getWarrantyMonths()));
         warrantyDAO.create(testWarranty);
+
+        Product product = new Product();
+        product.setOwner(testUser);
+        product.setProductName("Test Product");
+        product.setWarranty(testWarranty);
+        testWarranty.setProduct(product);
+        productDAO.create(product);
+
 
         scheduler.checkWarranties();
 
