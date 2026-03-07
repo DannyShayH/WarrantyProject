8c21f8132d6151d326b78f704b7d82d4d0f7bc78
DannyShayH <HalawiDD@gmail.com>
2026-03-07 14:40:40 +0100

    Refactored App.java, removed EntitiyManagerFactoryService, Moved ObjectMapperService and altered Pom for Javalin

--- src/main/java/app/App.java
+++ src/main/java/app/App.java
@@ -1,28 +1,46 @@
 package app;
 
+import app.config.HibernateConfig;
+import app.controllers.ProductController;
+import app.controllers.UserController;
+import app.controllers.WarrantyController;
+import app.routes.Routes;
 import app.services.notificationServices.SendGridService;
-import app.services.persistenceServices.EntityManagerFactoryService;
 import app.services.testClassService.TestClassFactory;
 import app.services.xmlServices.XmlService;
 import app.utils.Populator;
+import io.javalin.Javalin;
+import jakarta.persistence.EntityManagerFactory;
 
 public class App {
      static void initiate(){
+        final EntityManagerFactory emf = HibernateConfig.getEntityManagerFactory();
         XmlService xmlService = new XmlService();
-        Populator populater = new Populator(EntityManagerFactoryService.getEntityManagerFactory());
+//        Populator populater = new Populator();
+        ProductController pController = new ProductController(emf);
+        WarrantyController wController = new WarrantyController(emf);
+        UserController uController = new UserController(emf);
+        Routes routes = new Routes(pController, wController, uController);
 
         long start = System.currentTimeMillis();
 
-        populater.populateAndCreateEntities();
+//        populater.populateAndCreateEntities();
 
-        long end = System.currentTimeMillis();
-
-         xmlService.extractAndPrintXML();
+        xmlService.extractAndPrintXML();
 
         TestClassFactory.testClassWarranty();
 
         SendGridService.sendGridService();
 
+        var app = Javalin.create(config -> {
+           config.routes.apiBuilder(routes.getRoutes());
+           config.bundledPlugins.enableRouteOverview("routes");
+           config.routes.exception(RuntimeException.class, (e, ctx) -> {
+              ctx.status(400).json(e.getMessage());
+           });
+        }).start(7070);
+
+        long end = System.currentTimeMillis();
         System.out.println("All data fetched in " + (end - start) + " ms");
     }
 }
